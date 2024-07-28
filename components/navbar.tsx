771295daaaf49274aa77604667cf117b0b67f328
spidey0p <jyotiradityabiswal64@gmail.com>
2024-07-29 01:45:39 +0530

    adding a new font

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,28 @@
 "use client";
 
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    weight: "600",
+    subsets: ["latin"]
+});
+
 export const NavBar = () => {
     return (
-        <div>
-            NavBar
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+            <div className="flex items-center">
+                <Menu className="block md:hidden" />
+                <Link href="/">
+                    <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",
+                        font.className
+                    )}>
+                        companion.ai
+                    </h1>
+                </Link>
+            </div>
         </div>
     )
 }
