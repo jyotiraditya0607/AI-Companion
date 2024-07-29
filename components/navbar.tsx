df051117ba59fef8258355becf18f788060bbfa7
spidey0p <jyotiradityabiswal64@gmail.com>
2024-07-29 20:34:47 +0530

    adding theme botton

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { Menu, Sparkles } from "lucide-react";
 import Link from "next/link";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const font = Poppins({
     weight: "600",
@@ -22,6 +25,14 @@ export const NavBar = () => {
                         companion.ai
                     </h1>
                 </Link>
+            </div>
+            <div className="flex items-center gap-x-3">
+                <Button variant="premium" size="sm">
+                    Upgrade
+                    <Sparkles className="w-4 h-4 fill-white text-white ml-2" />
+                </Button>
+                <ModeToggle />
+                <UserButton />
             </div>
         </div>
     )
