e0e136b265cc3bb163184e2786bf0965a57886a8
spidey0p <jyotiradityabiswal64@gmail.com>
2024-08-01 21:13:17 +0530

    changes in components

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 const TestPage = () => {
     return (
         <div>
-            <UserButton afterSignOutUrl="/" />
+            <UserButton afterSignOutUrl="/"/>
         </div>
     );
 }
