267d61864705a18d05e7d85b01b24c916b022fac
spidey0p <jyotiradityabiswal64@gmail.com>
2024-08-03 20:29:56 +0530

    adding mobile-sidebar compo

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,26 @@
+
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger
+} from "@/components/ui/sheet";
+import { Sidebar } from "@/components/sidebar";
+import { Menu } from "lucide-react";
+
+
+export const MobileSidebar = () => {
+    return (
+        <div>
+            <Sheet>
+                <SheetTrigger className="md:hidden pr-4">
+                    <Menu />
+                </SheetTrigger>
+                <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+                    <Sidebar />
+                </SheetContent>
+            </Sheet>
+        </div>
+    )
+}
+
+
