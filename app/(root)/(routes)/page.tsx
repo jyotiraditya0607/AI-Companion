42f0a702fee589ece703790a2a41e05850629019
spidey0p <jyotiradityabiswal64@gmail.com>
2024-08-05 20:23:08 +0530

    adding categoties button

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,12 +1,19 @@
+import { Categories } from "@/components/categories";
 import { SearchInput } from "@/components/search-input";
-import { UserButton } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
 
-const TestPage = () => {
+
+const RootPage = async () => {
+    const categories = await prismadb.category.findMany();
+
     return (
         <div className="h-full p-4 space-y-2">
             <SearchInput />
+            <Categories data={categories} />
         </div>
     );
 }
 
-export default TestPage;
+
+
+export default RootPage;
